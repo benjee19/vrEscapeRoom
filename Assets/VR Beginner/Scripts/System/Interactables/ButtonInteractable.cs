5e9dea9fc1fcf8b56f083dbdfa339a8dda3eb945
Aditi Jain <aditijain@Aditis-Mac.local>
2023-06-01 12:08:15 -0700

    potion bottle

--- Assets/VR Beginner/Scripts/System/Interactables/ButtonInteractable.cs
+++ Assets/VR Beginner/Scripts/System/Interactables/ButtonInteractable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:492c9aafbb280d4458fa1cb2bd5a355152637118c27ad46d7308d76c54eec103
-size 2304
+oid sha256:b213d78d72d63ed6982f8e8a07a43a0977f3bf1e7394413d7aa5a394d36d36ed
+size 2421
